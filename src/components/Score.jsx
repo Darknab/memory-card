24f4abcd9c2dca3490cd6142ee559a23f6f3b54f
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-01 07:41:30 +0100

    Move the score to a separate component

---
+++ src/components/Score.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export default function Score({ score, bestScore }) {
+  return (
+    <div className="score">
+      <p>Score: {score}</p>
+      <p>Best Score: {bestScore}</p>
+  </div>
+  )
+}
+
+Score.propTypes = {
+  score: PropTypes.number.isResuired,
+  bestScore: PropTypes.number.isRequired,
+}
